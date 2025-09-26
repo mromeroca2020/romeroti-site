dd58a2416881c12f7efb4c91824d5532209e2b3c
mromeroca2020 <58543051+mromeroca2020@users.noreply.github.com>
2025-09-26 11:40:05 -0400

    Update run-tool.js

--- netlify/functions/run-tool.js
+++ netlify/functions/run-tool.js
@@ -1,1 +1,10 @@
-
+// netlify/functions/run-tool.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      message: '✅ Función ejecutada con éxito',
+      output: 'Esta es una prueba desde Netlify Functions.' 
+    })
+  };
+}
