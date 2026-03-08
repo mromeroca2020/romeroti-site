0653f88d491dc9709d4df61d95d9f569f5f734ee
mromeroca2020 <58543051+mromeroca2020@users.noreply.github.com>
2026-03-08 00:18:04 -0500

    Update send-booking.js

--- netlify/functions/send-booking.js
+++ netlify/functions/send-booking.js
@@ -1,1 +1,43 @@
+const nodemailer = require("nodemailer");
 
+exports.handler = async (event) => {
+
+const data = JSON.parse(event.body);
+
+const transporter = nodemailer.createTransport({
+host: "mail.privateemail.com",
+port: 587,
+secure: false,
+auth: {
+user: "info@romanoti-solutions.com",
+pass: process.env.EMAIL_PASS
+}
+});
+
+const message = `
+New Consultation Request
+
+Name: ${data.fullname}
+Email: ${data.email}
+Phone: ${data.phone}
+Company: ${data.company}
+Service: ${data.service}
+Date: ${data.date}
+Time: ${data.time}
+
+Issue:
+${data.issue}
+`;
+
+await transporter.sendMail({
+from: '"Romanoti Booking" <info@romanoti-solutions.com>',
+to: "info@romanoti-solutions.com",
+subject: "New Consultation Request",
+text: message
+});
+
+return {
+statusCode: 200,
+body: JSON.stringify({status:"sent"})
+};
+};
